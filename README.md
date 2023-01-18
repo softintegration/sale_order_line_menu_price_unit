807ce81f56ee9fbb69e3ff0f868297aa66857bea
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-18 10:12:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Sale order line menu with price unit
+------------------------------------
+This module display the price unit field in sale order line tree views.
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale order line menu with price unit',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale',
+    'description': "",
+    'depends': [
+        'sale_order_line_menu'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
